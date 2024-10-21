8b9da52e7044759f6ed85e1254444f4ad19c2166
thiagodeas <sousaalvesth@gmail.com>
2024-10-21 09:29:55 -0300

    inicializando o projeto

---
+++ src/test/java/com/thiagodeas/curriculum_maker/CurriculumMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagodeas.curriculum_maker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculumMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
