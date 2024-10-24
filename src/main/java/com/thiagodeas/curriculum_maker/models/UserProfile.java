52deabed14dfdb1261789120df52dfc6998197ee
thiagodeas <sousaalvesth@gmail.com>
2024-10-24 16:24:02 -0300

    refatorando campos do modelo UserProfile

--- src/main/java/com/thiagodeas/curriculum_maker/models/UserProfile.java
+++ src/main/java/com/thiagodeas/curriculum_maker/models/UserProfile.java
@@ -20,11 +20,11 @@ public class UserProfile {
     private String linkedin;
     private String github;
     private String aboutMe;
-    private String frontEndSkills;
-    private String backEndSkills;
-    private String databaseSkills;
-    private String cloudSkills;
-    private String otherSkills;
+    private String frontEndKnowledge;
+    private String backEndKnowledge;
+    private String databaseKnowledge;
+    private String cloudKnowledge;
+    private String othersKnowledge;
     private List<Education> education;
     private List<Project> projects;
     private List<AdditionalActivity> additionalActivities;
