42c40e70374c21bf28fa2643f2434859ffa824c4
thiagodeas <sousaalvesth@gmail.com>
2025-05-19 10:27:24 -0300

    first commit

---
+++ src/main/java/com/thiagodeas/curriculum_maker/CurriculumMakerApplication.java
@@ -0,0 +1,13 @@
+package com.thiagodeas.curriculum_maker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculumMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculumMakerApplication.class, args);
+	}
+
+}
